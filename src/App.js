fb4fd6b741cf24afd87e9f09fae4f7ba16215cf2
yeomboyeon <yeomboyeon1221@gmail.com>
2022-11-23 21:08:46 +0900

    mysql 연동, 로그인, 회원가입 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import axios from "axios";
+import Login from "./pages/login.js";
+import Join from "./pages/join.js";
+
+//session 설정 후 도메인이 다른 포트 번호와 쿠키 공유가 안되는데 공유 가능해짐
+axios.defaults.withCredentials = true;
+
+console.clear();
+/**1. React router dom 설치 및 설정
+ * 2. Constext Provider 설정
+ * 3. 회원가입 설정
+ * 4. 서버 연동
+ * 5. 로그인 설정
+ * 6. 세션(Session) 저장 및 설정
+ * 7. 서버 연동
+ * 8. mysql 연동
+ * 9. 세션정보 가져오기
+ */
+
+function Main() {
+  return <div>Main</div>;
+}
+
+const StoreContext = React.createContext({});
 
 function App() {
+  const [loginUser, setLoginUser] = React.useState({});
+
+  const 세션정보가져오기 = async () => {
+    await axios({
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      setLoginUser(res.data);
+    });
+  };
+
+  React.useEffect(() => {
+    세션정보가져오기();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreContext.Provider value={{ loginUser }}>
+      <Routes>
+        <Route exact path="/" element={<Main />}></Route>
+        <Route exact path="/join" element={<Join />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+      </Routes>
+    </StoreContext.Provider>
   );
 }
 
---
+++ src/pages/join.js
@@ -0,0 +1,70 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+// 3번 회원가입 설정
+// useState 저장(필요한 데이터는 id, pw를 객체화)
+// 회원가입하고 메인 페이지로 이동하기(네비게이션)
+function Join() {
+  const navigation = useNavigate();
+
+  // 최상위에서 호출하기
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  // 이벤트로 데이터 변경값 저장
+  const 데이터변경 = (event) => {
+    // alert(event.target.name); // id, pw가 선택됨
+    const name = event.target.name;
+
+    const cloneData = { ...data };
+
+    // id, pw 모두 입력된 값을 받아와야하기에 name로 저장한 값을 배열로 받기
+    // event.target.value 내가 입력한 값이 cloneData[name]이다.
+    cloneData[name] = event.target.value;
+    // console.log(cloneData[name]); 저장되는지 확인
+
+    // 변경된 데이터 값 저장
+    setData(cloneData);
+  };
+
+  // axios 서버 연결
+  // 생성이기에 POST 연결, data 받기
+  // return 값에 나올 값들이 서버로 넘겨져야 한다.
+  // GET 가져온다.(로그인) POST 만든다.(회원가입)
+  // POST 는 data로 보내기
+  const 회원가입 = async () => {
+    await axios({
+      url: "http://localhost:4000/join",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        const { code, message } = res.data;
+
+        if (code === "success") {
+          alert(message);
+          navigation("/login");
+        }
+      })
+      .catch((error) => {
+        console.log("join 에러", error);
+      });
+    console.log(data);
+  };
+
+  // name 활용, event.target.name으로 받은 데이터를 onChange{데이터변경}로 받아오기
+  return (
+    <div>
+      <input type="text" name="id" onChange={데이터변경} />
+      <input type="password" name="pw" onChange={데이터변경} />
+      <button type="button" onClick={회원가입}>
+        회원가입
+      </button>
+    </div>
+  );
+}
+
+export default Join;
